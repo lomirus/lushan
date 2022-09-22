40ce393b1e602aa9ec2bd748a47cfbcb25405224
lumingrui <lomirus@qq.com>
2022-09-22 16:38:00 +0800

    ✨ Create canvas and basic render function

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,17 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { pixelateCharacter } from './utils';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const data = pixelateCharacter('翰');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const canvas = document.querySelector('canvas')!;
+const ctx = canvas.getContext('2d');
+if (!ctx) throw "Context is null";
+
+for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data.length; j++) {
+        if (data[i][j]) {
+            ctx.fillRect(j, i, 1, 1)
+        }
+    }
+}
+
---
+++ src/utils/index.ts
@@ -0,0 +1,25 @@
+const FONT_SIZE = 256;
+
+export function pixelateCharacter(char: string): Array<Array<boolean>> {
+    // Initialize Canvas
+    const canvas = document.createElement('canvas');
+    canvas.width = FONT_SIZE;
+    canvas.height = FONT_SIZE;
+
+    // Initialize Context
+    const ctx = canvas.getContext('2d')!;
+    ctx.textBaseline = 'bottom'
+    ctx.font = `${FONT_SIZE}px SourceHanSerif`;
+
+    // Calculate the pixel data
+    ctx.fillText(char, 0, FONT_SIZE);
+    const imageData = ctx.getImageData(0, 0, FONT_SIZE, FONT_SIZE)
+    const pixelData: boolean[][] = new Array(FONT_SIZE)
+    for (let i = 0; i < FONT_SIZE; i++) {
+        pixelData[i] = new Array(FONT_SIZE)
+        for (let j = 0; j < FONT_SIZE; j++) {
+            pixelData[i][j] = imageData.data[3 + 4 * (FONT_SIZE * i + j)] > 127
+        }
+    }
+    return pixelData
+}
